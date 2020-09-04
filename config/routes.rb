49f7c1b799d81dbf20f13d50452d08a1c6844ccb
“Krista <stadler.krista@gmail..com>
2020-09-04 09:08:41 -0600

    Got form setup for creating new book by ISBN

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/books/new", to: "books#new"
+  post "/books", to: "books#create"
 end
