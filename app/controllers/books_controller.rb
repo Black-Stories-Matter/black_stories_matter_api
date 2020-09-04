5658097f4884a74a0b7a7e1d4c0f78306ccd9a5a
“Krista <stadler.krista@gmail..com>
2020-09-04 09:32:25 -0600

    Got API call working and creating new books and authors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,5 +4,24 @@ class BooksController < ApplicationController
   end
 
   def create
-  end 
+    response = Faraday.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{params[:isbn]}")
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    #need to go back and change relationship to many to many to add authors to books
+    json_authors = json[:items][0][:volumeInfo][:authors]
+    book_authors = []
+    json_authors.each do |author|
+      book_authors << Author.find_or_create_by(name: author)
+    end
+
+    book = Book.create(isbn: params[:isbn],
+                       title: json[:items][0][:volumeInfo][:title],
+                       cover_image: json[:items][0][:volumeInfo][:imageLinks][:thumbnail],
+                       description: json[:items][0][:volumeInfo][:description],
+                       publication_date: json[:items][0][:volumeInfo][:publishedDate],
+                       category: json[:items][0][:volumeInfo][:categories][0],
+                       maturity: json[:items][0][:volumeInfo][:maturityRating],
+                       info_link: json[:items][0][:volumeInfo][:infoLink])
+
+  end
 end
