24537841bbcce1d3b4e69a2747b2ee91f1185cef
“Krista <stadler.krista@gmail..com>
2020-09-18 21:27:15 -0600

    Removed pry from spec file

--- spec/requests/api/v1/book_requests_spec.rb
+++ spec/requests/api/v1/book_requests_spec.rb
@@ -14,6 +14,5 @@ describe 'Books API' do
     get "/api/v1/books"
     results = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
-    require "pry"; binding.pry
   end
 end
