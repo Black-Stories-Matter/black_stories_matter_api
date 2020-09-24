38df751ad714f91a9ebaa94a88e862f666305248
“Krista <stadler.krista@gmail..com>
2020-09-24 10:11:26 -0600

    Added validation to ISBN length

--- spec/features/new_book_creation_spec.rb
+++ spec/features/new_book_creation_spec.rb
@@ -13,24 +13,39 @@ RSpec.describe "As a visitor,", type: :feature do
       book = Book.last
       expect(page).to have_content("You succesfully added a book to the database")
       expect(current_path).to eq("/books/#{book.id}")
-
     end
 
     it "doesn't allow you to enter the same book twice" do
-    visit '/books/new'
+      visit '/books/new'
 
-    fill_in :isbn, with: "0439023440"
+      fill_in :isbn, with: "0439023440"
 
-    click_on "Submit"
+      click_on "Submit"
 
-    visit '/books/new'
+      visit '/books/new'
 
-    fill_in :isbn, with: "0439023440"
+      fill_in :isbn, with: "0439023440"
 
-    click_on "Submit"
+      click_on "Submit"
 
-    expect(page).to have_content("Book not created, please try again")
+      expect(page).to have_content("Book not created, please try again")
+    end
 
-    end 
+    it "doesn't allow you to enter an ISBN that is longer than 10 digits" do
+
+      visit '/books/new'
+
+      fill_in :isbn, with: "043902344045"
+
+      click_on "Submit"
+
+      expect(page).to have_content("We could not complete your request. Please make sure you entered an ISBN-10 with no dashes.")
+
+      fill_in :isbn, with: "04-3902-3440"
+
+      click_on "Submit"
+
+      expect(page).to have_content("We could not complete your request. Please make sure you entered an ISBN-10 with no dashes.")
+    end
   end
 end
