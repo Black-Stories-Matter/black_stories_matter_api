cead2c7ee567f1c61fcf9554756d50ab1902baf6
“Krista <stadler.krista@gmail..com>
2020-09-24 14:08:29 -0600

    Added delete functionality to API

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   validates :isbn, uniqueness: true, presence: true
   validates_presence_of :title
-  has_many :author_books
+  has_many :author_books, dependent: :destroy
   has_many :authors, through: :author_books
 end
