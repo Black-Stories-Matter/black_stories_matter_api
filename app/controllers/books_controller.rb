d6a81ef62d48193c2dc93a1b653a043313379834
“Krista <stadler.krista@gmail..com>
2020-09-28 08:51:58 -0600

    Refactored book service

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,24 +14,8 @@ class BooksController < ApplicationController
 
   def create
     if params[:isbn].length == 10
-      response = Faraday.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{params[:isbn]}")
-      json = JSON.parse(response.body, symbolize_names: true)
-
-      book = Book.create(isbn: params[:isbn],
-                      title: json[:items][0][:volumeInfo][:title],
-                      cover_image: json[:items][0][:volumeInfo][:imageLinks][:thumbnail],
-                      description: json[:items][0][:volumeInfo][:description],
-                      publication_date: json[:items][0][:volumeInfo][:publishedDate],
-                      category: json[:items][0][:volumeInfo][:categories][0],
-                      maturity: json[:items][0][:volumeInfo][:maturityRating],
-                      info_link: json[:items][0][:volumeInfo][:infoLink])
-
-      json_authors = json[:items][0][:volumeInfo][:authors]
-      json_authors.each do |author|
-        author_object = Author.find_or_create_by(name: author)
-        AuthorBook.create(author_id: author_object.id, book_id: book.id)
-      end
-
+      book = BookService.new.create_book(params[:isbn])
+      authors = BookService.new.create_author(params[:isbn], book)
       if book.save
         redirect_to "/books/#{book.id}"
         flash[:success] ="You succesfully added a book to the database"
