8daef4a8cc15b981db7247590fd18654a7f8c7b4
“Krista <stadler.krista@gmail..com>
2020-10-15 09:15:07 -0600

    Still trying to fix CORS

--- config/application.rb
+++ config/application.rb
@@ -32,10 +32,10 @@ module BlackStoriesMatterApi
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://localhost:3000'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
-      end
+      # allow do
+      #   origins 'http://localhost:3000'
+      #   resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      # end
       allow do
         origins 'https://blackstoriesmatter.herokuapp.com'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
